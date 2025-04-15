6bb7f174ca1fe0d93ea69e7da6223102f6cd4a07
Danny <gh@hdcola.org>
2025-04-15 11:50:44 -0400

    init project

---
+++ README.md
@@ -0,0 +1,32 @@
+# Fill Agent Server
+
+
+## Setup
+
+1. Clone the repository:
+2. Init venv and install dependencies:
+```
+uv venv
+uv sync
+```
+
+## Run
+
+### Run a test web server
+
+```
+adk web
+```
+
+### Run a cli 
+
+```
+adk run income_tax_agent
+```
+
+
+### Run a API Server
+
+```
+adk api_server
+```
---
+++ income_tax_agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+from .base_info import get_user_data_tool, update_user_data_tool
+
+root_agent = Agent(
+    name="IncomeTaxAgent",
+    model="gemini-2.0-flash",
+    description="You are a tax agent. You can help users fill out their tax returns.",
+    instruction=(
+        "You are a tax agent. You can help users fill out their tax returns. "
+        "You can answer questions about tax returns, provide information about tax laws, and assist with the filing process. "
+    ),
+    tools=[get_user_data_tool, update_user_data_tool],
+)
